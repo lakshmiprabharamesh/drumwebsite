b2aacb152140698aed35b1b1d720400c814d9c64
lakshmiprabharamesh <47028995+lakshmiprabharamesh@users.noreply.github.com>
2020-04-27 01:17:07 +0530

    DRUM WEBSITE FILES

---
+++ index.js
@@ -0,0 +1,95 @@
+
+
+// Button Press
+var numberofdrumbottles=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberofdrumbottles;i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+var buttonInnerhtml= this.innerHTML;
+
+ makesound(buttonInnerhtml);
+ buttonanimation(buttonInnerhtml);
+
+
+});
+}
+
+
+
+// Keyboard press
+
+document.addEventListener("keypress",function(event){
+  makesound(event.key);
+
+ buttonanimation(event.key);
+
+} );
+
+function makesound(key){
+
+  switch (key) {
+    case "w":
+        var tom1=new Audio("tom-1.mp3");
+        tom1.play();
+    break;
+
+   case "a":
+       var tom2=new Audio("tom-2.mp3");
+       tom2.play();
+  break;
+
+  case "s":
+      var tom3=new Audio("tom-3.mp3");
+      tom3.play();
+  break;
+
+  case "k":
+      var tom4=new Audio("tom-4.mp3");
+      tom4.play();
+  break;
+
+  case "j":
+      var crash=new Audio("crash.mp3");
+      crash.play();
+  break;
+
+  case "d":
+      var snare=new Audio("snare.mp3");
+      snare.play();
+  break;
+
+  case "l":
+      var kick=new Audio("kick-bass.mp3");
+      kick.play();
+  break;
+
+
+    default:console.log("buttonInnerhtml");
+
+
+
+}
+
+}
+
+
+function buttonanimation(currentkey){
+
+var activate=document.querySelector("." + currentkey);
+
+activate.classList.add("pressed");
+
+
+setTimeout(function(){
+activate.classList.remove("pressed");
+
+},100);
+// activate.classList.add("pressed");
+
+}
+
+// var audio=new Audio("sounds/tom-1.mp3");
+// audio.play();
+//
